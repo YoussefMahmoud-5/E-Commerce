90bf400c830cb7ecfd9c5825de6ab45fa76a6a9d
YOUSEF <YOUSEF@DESKTOP-HOIHUBF>
2025-10-07 19:44:04 +0300

    Add project files.

---
+++ Infastructure/Persistence/Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Data.Contexts
+{
+    public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+        }
+    }
+}
---
+++ Infastructure/Persistence/DataSeeding.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data.Contexts;
+
+namespace Persistence
+{
+    public class DataSeeding(StoreDbContext _dbContext) : IDataSeeding
+    {
+        public void DataSeed()
+        {
+            try
+            {
+                if (_dbContext.Database.GetPendingMigrations().Any())
+                {
+                    _dbContext.Database.Migrate();
+                }
+
+                if (!_dbContext.ProductBrands.Any())
+                {
+                    var productBrandsData = File.ReadAllText(@"..\Infastructure\Persistence\Data\DataSeed\brands.json");
+                    var productBrand = JsonSerializer.Deserialize<List<ProductBrand>>(productBrandsData);
+                    if (productBrand is not null && productBrand.Any())
+                    {
+                        _dbContext.ProductBrands.AddRange(productBrand);
+                    }
+                }
+
+                if (!_dbContext.ProductTypes.Any())
+                {
+                    var productTypeData = File.ReadAllText(@"..\Infastructure\Persistence\Data\DataSeed\types.json");
+                    var productType = JsonSerializer.Deserialize<List<ProductType>>(productTypeData);
+                    if (productType is not null && productType.Any())
+                    {
+                        _dbContext.ProductTypes.AddRange(productType);
+                    }
+                }
+                if (!_dbContext.Products.Any())
+                {
+                    var productData = File.ReadAllText(@"..\Infastructure\Persistence\Data\DataSeed\products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productData);
+                    if (products is not null && products.Any())
+                    {
+                        _dbContext.Products.AddRange(products);
+                    }
+                }
+                _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                //TO Do
+            }
+            
+        }
+    }
+}
